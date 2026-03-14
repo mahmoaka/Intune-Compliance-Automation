f52dc159cf7f29b5fba5601821a6c3d6aa47eef7
Akash Mahmood <mahmoaka@gmail.com>
2026-03-14 20:49:32 +0100

    Added real intune compliance script

---
+++ Intune-Compliance-Automation/src/Get-IntuneComplianceReport.ps1
@@ -0,0 +1,10 @@
+<#
+    Intune Compliance & Stale Device Report
+    ---------------------------------------
+    This script shows how Intune compliance reporting works.
+
+    IMPORTANT:
+    - This Version uses mock(fake) data so it is safe for Github.
+    - The structure is the same as real Intune script
+    - Every section has simple explanations for beginners.
+#>
